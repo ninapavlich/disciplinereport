c6fdeeb16a2738dae43487eaa7d7222db9d1d2cc
Nina Pavlich <nina@ninalp.com>
2016-07-18 02:30:57 -0400

    fix map for districts

--- disciplinereport/apps/data/models.py
+++ disciplinereport/apps/data/models.py
@@ -53,24 +53,10 @@ class BaseEntity(BasePage, StreetAddressAtom):
         return u'https://www.google.com/maps/place/%s,+%s,+%s+%s/%s,%s'%(self.street_1, self.city, self.state, self.zipcode, self.latitude, self.longitude)
 
     def get_google_map_image_url(self):
+
         return u'https://maps.googleapis.com/maps/api/staticmap?center=%s,%s&zo\
-om=7&scale=2&size=400x200&maptype=roadmap&format=png&visual_refresh=\
-true&markers=color:0x0077bd|label:|%s,%s&style=feature:water|element:\
-geometry|color:0xe9e9e9|lightness:17|&style=feature:landscape|element\
-:geometry|color:0xf5f5f5|lightness:20|&style=feature:road.highway|ele\
-ment:geometry.fill|color:0xffffff|lightness:17|&style=feature:road.hi\
-ghway|element:geometry.stroke|color:0xffffff|lightness:29|weight:0.2|\
-&style=feature:road.arterial|element:geometry|color:0xffffff|lightnes\
-s:18|&style=feature:road.local|element:geometry|color:0xffffff|lightn\
-ess:16|&style=feature:poi|element:geometry|color:0xf5f5f5|lightness:2\
-1|&style=feature:poi.park|element:geometry|color:0xdedede|lightness:2\
-1|&style=element:labels.text.stroke|visibility:on|color:0xffffff|ligh\
-tness:16|&style=element:labels.text.fill|saturation:36|color:0x333333\
-|lightness:40|&style=element:labels.icon|visibility:off|&style=featur\
-e:transit|element:geometry|color:0xf2f2f2|lightness:19|&style=feature\
-:administrative|element:geometry.fill|color:0xfefefe|lightness:20|&st\
-yle=feature:administrative|element:geometry.stroke|color:0xfefefe|lig\
-        htness:17|weight:1.2|'%(self.latitude, self.longitude, self.latitude, self.longitude)
+om=10&scale=false&size=400x250&maptype=roadmap&format=png&visual_refresh=\
+true&markers=color:0x85200c|label:|%s,%s'%(self.latitude, self.longitude, self.latitude, self.longitude)
 
     class Meta:
         abstract = True
