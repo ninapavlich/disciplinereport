c4144d6ed3357ab897bc3bdecad69c4e8c0a0624
Nina Pavlich <nina@ninalp.com>
2016-07-24 23:51:00 -0400

    update local db settings

--- disciplinereport/settings/local.py
+++ disciplinereport/settings/local.py
@@ -12,18 +12,12 @@ ALLOWED_HOSTS = ['*']
 #==============================================================================
 # LOCAL DATBASE
 #==============================================================================
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(VAR_ROOT, 'disciplinereport'),
-#     },
-# }
 
 DATABASES = {
      'default': {
-        'ENGINE': env.get("DB_DRIVER"),
+        'ENGINE': env.get("DB_DRIVER", 'django.db.backends.sqlite3'),
         'HOST': env.get("DB_HOST"),
-        'NAME': env.get("DB_NAME"),
+        'NAME': env.get("DB_NAME", os.path.join(VAR_ROOT, 'disciplinereport')),
         'USER': env.get("DB_USER"),
         'PASSWORD': env.get("DB_PASSWORD"),
         'PORT': env.get("DB_PORT"),
